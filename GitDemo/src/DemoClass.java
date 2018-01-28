e78aa2003977c951e7e672346be94641569c3480
nikhil <nikhil@DESKTOP-3LFMDB5>
2018-01-28 17:43:23 +0530

    my first commit

---
+++ GitDemo/src/DemoClass.java
@@ -0,0 +1,11 @@
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String name = "My name is nikhil ";
+		System.out.println(name);
+	}
+
+}
